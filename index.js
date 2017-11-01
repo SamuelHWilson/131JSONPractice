0d11c1ee14a2262e575f5e6f1cf9f9cbd230b277
BRADEN <vb0517423@otc.edu>
2017-11-01 09:24:27 -0500

    Pushy lodash

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ var fs = require("fs");
 var contents = fs.readFileSync("data.json");
 var people = JSON.parse(contents);
 
-console.log(people.items[0].fname);
+//console.log(people.items[0].fname);
+
+_.forEach(people.items[0], value =>{
+    console.log(value);
+});
+
+_.forEach(people.items[1], value =>{
+    console.log(value);
+});
